7b65f14c5980459e0bd0e2b6a78f4a5b317dc78a
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-12-21 12:34:54 -0800

    Rename

---
+++ StudyApplicationTests/StudyApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+// This source file is part of the Stanford Spezi Study Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import StudyApplication
+import XCTest
+
+
+class StudyApplicationTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
