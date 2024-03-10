e0d7defc41a1c2e3eb69e6e528c23720b9029424
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-03-10 00:56:06 -0800

    Fix Import

--- StudyApplication/StudyApplicationDelegate.swift
+++ StudyApplication/StudyApplicationDelegate.swift
@@ -12,7 +12,6 @@ import SpeziFirebaseAccount
 import SpeziFirebaseStorage
 import SpeziFirestore
 import SpeziHealthKit
-import SpeziMockWebService
 import SpeziOnboarding
 import SpeziScheduler
 import SwiftUI
