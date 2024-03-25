f254b3968067455be498fc15f473c5d9b1d1bb71
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-03-25 08:25:13 -0700

    Start Active Tasks

--- StudyApplication/Schedule/StudyApplicationTaskContext.swift
+++ StudyApplication/Schedule/StudyApplicationTaskContext.swift
@@ -10,25 +10,43 @@ import Foundation
 import ModelsR4
 
 
+struct ActiveTask {
+    let id: UUID
+    let localizedDescription: LocalizedStringResource
+}
+
+
 /// The context attached to each task in the Spezi Study Application.
 ///
 /// We currently only support `Questionnaire`s, more cases can be added in the future.
 enum StudyApplicationTaskContext: Codable, Identifiable {
     /// The task should display a `Questionnaire`.
     case questionnaire(Questionnaire)
+    /// The task should display an activity task.
+    case activeTask(Questionnaire)
         
     
-    var id: FHIRPrimitive<FHIRString>? {
+    var id: String {
         switch self {
         case let .questionnaire(questionnaire):
-            return questionnaire.id
+            guard let id = questionnaire.id?.primitiveDescription else {
+                fatalError("Could not derive ID from questionnaire.")
+            }
+            return id
+        case let .questionnaire(questionnaire):
+            guard let id = questionnaire.id?.primitiveDescription else {
+                fatalError("Could not derive ID from questionnaire.")
+            }
+            return id
         }
     }
     
     var actionType: LocalizedStringResource {
         switch self {
         case .questionnaire:
-            return LocalizedStringResource("TASK_CONTEXT_ACTION_QUESTIONNAIRE")
+            return LocalizedStringResource("Start Questionnaire")
+        case .activeTask:
+            return LocalizedStringResource("Start Active Task")
         }
     }
 }
